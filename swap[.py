8dd654040fc712e5e44cf676944b34d0ae05b429
suprith <suprithsp7@gmail.com>
2025-06-19 15:54:20 +0530

    Create swap[.py

---
+++ swap[.py
@@ -0,0 +1,17 @@
+# Swap two numbers using a temporary variable
+
+a = 5
+b = 10
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Using a temporary variable to swap
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
